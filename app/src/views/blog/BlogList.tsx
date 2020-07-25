aabcdc22c9e4bd8c14842ae386df664023b0c1fa
Shane Smith <voodoogq@gmail.com>
2020-07-25 16:00:07 -0500

    BlogList

--- app/src/views/blog/BlogList.tsx
+++ app/src/views/blog/BlogList.tsx
@@ -4,38 +4,32 @@ import { Header, Icon } from "semantic-ui-react";
 import data from "./blog.json";
 import "./BlogList.scss";
 
-interface Props {}
-
-class BlogList extends React.Component<Props> {
-  renderItems() {
-    return data.articles.map((article) => {
-      return (
-        <li key={article.slug} className="blog-item">
-          <a href={`/blog/${article.slug}`}>{article.title}</a> -{" "}
-          <span className="blog-date">{article.date}</span>
-        </li>
-      );
-    });
-  }
-
-  render() {
-    return (
-      <div className="blog-list">
-        <Header
-          as="h1"
-          textAlign="center"
-          icon
-          inverted
-          size="huge"
-          className="page-header"
-        >
-          <Icon name="newspaper outline" circular inverted color="red" />
-          <Header.Content className="page-header-content">Blog</Header.Content>
-        </Header>
-        <ul>{this.renderItems()}</ul>
-      </div>
-    );
-  }
-}
+const BlogList: React.FC = (): JSX.Element => {
+  return (
+    <div className="blog-list">
+      <Header
+        as="h1"
+        textAlign="center"
+        icon
+        inverted
+        size="huge"
+        className="page-header"
+      >
+        <Icon name="newspaper outline" circular inverted color="red" />
+        <Header.Content className="page-header-content">Blog</Header.Content>
+      </Header>
+      <ul>
+        {data.articles.map((article) => {
+          return (
+            <li key={article.slug} className="blog-item">
+              <a href={`/blog/${article.slug}`}>{article.title}</a> -{" "}
+              <span className="blog-date">{article.date}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default connect(null, null)(BlogList);
